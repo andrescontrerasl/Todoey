050e3bfa9647cfbd8d9fd47c0e4bf846edb21b75
acontreraslama <andrescontreras@andress-macbook-pro-10.local>
2018-10-30 14:34:50 +1000

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Andres Contreras on 30/10/18.
+//  Copyright © 2018 Andres Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
